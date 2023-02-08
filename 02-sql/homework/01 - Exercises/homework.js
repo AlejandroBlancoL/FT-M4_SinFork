e2147fa5cadaf9e7fa79094179327188d2ef0998
AlejandroBlancoL <candoblanco2003@gmail.com>
2023-02-07 23:14:23 -0600

    commit2

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,16 +1,16 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT title FROM movies  WHERE duration < 91;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT title FROM movies WHERE year > 1928 AND year < 1941;';
 
-const ejercicio04 = '';
+const ejercicio04 = 'SELECT title FROM movies WHERE title LIKE '%til%';';
 
 const ejercicio05 = '';
 
 const ejercicio06 = '';
 
-const ejercicio07 = '';
+const ejercicio07 = 'SELECT title FROM movies WHERE title LIKE '%Fast-and%' AND year = 2016;';
 
 module.exports = {
    ejercicio02,
